85a4aa6ad8e9473ed41875da5cae1386c3802e6e
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-07-15 11:17:46 +0900

    스프링 시큐리티 설정

---
+++ src/main/java/com/example/myplace/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.myplace.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/", "/home/**", "/member/register", "/css/**", "/image/**", "/js/**", "/images/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/member/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username, password, enabled "
+                        + "from tb_member "
+                        + "where username = ?")
+                .authoritiesByUsernameQuery("select username, name "
+                        + "from "
+                            + "tb_member_role mr join tb_member m on mr.member_id = m.member_id "
+                            + "join tb_role r on mr.role_id = r.role_id "
+                        + "where username = ?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
