79277b5dbac8e3b89f9af5ae03692626a4ca6de4
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-07-15 11:46:40 +0900

    홈 컨트롤러 경로 수정

--- src/main/java/com/example/myplace/controller/HomeController.java
+++ src/main/java/com/example/myplace/controller/HomeController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
+
+    @RequestMapping("/")
+    public String redirectHome() {
+        return "redirect:/home";
+    }
 
     @RequestMapping("/home")
     public String home() {
-        return home();
+        return "home";
     }
 }
