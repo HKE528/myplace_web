78e6fdeef74fe67d34ee45a46455eadbdf9bfa33
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-07-16 10:39:16 +0900

    지도 관련 자바 스크립트 생성

---
+++ src/main/resources/static/js/map.js
@@ -0,0 +1,12 @@
+const map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([127.72800919741388, 37.878953230304404]),
+        zoom: 15
+    })
+});
