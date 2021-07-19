27ab76571271223fff0aef638752f78b1738e3a7
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-07-19 11:56:43 +0900

    리스트 목록 불러오기 작성

--- src/main/java/com/example/myplace/controller/HomeController.java
+++ src/main/java/com/example/myplace/controller/HomeController.java
@@ -1,10 +1,20 @@
 package com.example.myplace.controller;
 
+import com.example.myplace.domain.Place;
+import com.example.myplace.domain.dto.PlaceDTO;
+import com.example.myplace.service.PlaceService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final PlaceService placeService;
 
     @RequestMapping("/")
     public String redirectHome() {
@@ -12,7 +22,15 @@ public class HomeController {
     }
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model model, Principal principal) {
+        System.out.println("Home!");
+
+        if(principal != null) {
+            List<PlaceDTO> allMyPlace = placeService.findAllMyPlace(principal.getName());
+
+            model.addAttribute("places", allMyPlace);
+        }
+
         return "home";
     }
 }
