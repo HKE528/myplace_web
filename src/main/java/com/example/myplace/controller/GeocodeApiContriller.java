d7fff2e3f7c5e9adf349c3cb15cf9ae88bb6978b
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-07-20 11:11:06 +0900

    좌표 주소 변환 API 작성

---
+++ src/main/java/com/example/myplace/controller/GeocodeApiContriller.java
@@ -0,0 +1,39 @@
+package com.example.myplace.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/geocode")
+public class GeocodeApiContriller {
+    private final String geocodeKey = "&key=5F043B4C-BC18-31D0-B652-0151881CD4C3";
+    private String baseURL = "http://api.vworld.kr/req/address?service=address&request=getAddress&type=ROAD";
+
+    @GetMapping("/address")
+    public String pointToAddress(@RequestParam("point") String point) throws Exception {
+        String coordAttr ="&point=" + point;
+        String reqURL = baseURL + geocodeKey + coordAttr;
+        String address = "";
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+
+            Map getUrlResult = restTemplate.getForObject(reqURL, Map.class);
+
+            Map response = (Map)getUrlResult.get("response");
+            ArrayList result = (ArrayList)response.get("result");
+            address = ((Map)result.get(0)).get("text").toString();
+
+        } catch (Exception e) {
+            address = "해당 주소를 찾을 수 없습니다.";
+        }
+
+        return address;
+    }
+}
