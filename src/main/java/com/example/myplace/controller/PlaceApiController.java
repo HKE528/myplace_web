b5525e9e6b42b994c76ef3febe2ed400a3069419
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-07-22 09:13:49 +0900

    이미지 업로드 하도록 수정

--- src/main/java/com/example/myplace/controller/PlaceApiController.java
+++ src/main/java/com/example/myplace/controller/PlaceApiController.java
@@ -1,20 +1,25 @@
 package com.example.myplace.controller;
 
 import com.example.myplace.domain.dto.PlaceDTO;
+import com.example.myplace.service.FileService;
 import com.example.myplace.service.PlaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/place")
 public class PlaceApiController {
     private final PlaceService placeService;
+    private final FileService fileService;
 
     @PostMapping("/add")
-    public String addPlace(PlaceDTO placeDTO, Principal principal) {
+    public Long addPlace(@RequestParam(value = "imageFiles", required = false) List<MultipartFile> files,
+                         PlaceDTO placeDTO, Principal principal) {
         Long id;
         String username = principal.getName();
 
@@ -27,9 +32,13 @@ public class PlaceApiController {
             id = placeService.updatePlace(placeDTO);
         }
 
-        System.out.println(id);
+        if(id != null && files != null) {
+            fileService.saveFile(username, id, files);
+        } else if(files == null) {
+            System.out.println(id + " : file is Null!!!");
+        }
 
-        return id.toString();
+        return id;
     }
 
     @GetMapping("/view/{id}")
